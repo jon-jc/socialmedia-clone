854541afe05e0e7e2a6255ecaf60232ce3bbe164
jon-jc <jonathancho.jc@gmail.com>
2024-02-22 12:44:02 -0800

    added Model and Button components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 import { AppProps } from "next/app";
 import Layout from "@/components/Layout";
+import Model from "@/components/Model";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
+      <Model actionLabel="submit" isOpen title="test" />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    
+      </>
   )
 }
