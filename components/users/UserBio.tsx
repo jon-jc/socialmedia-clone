f208fca6c971d1fec18a493f043ce702cea5b62b
jon-jc <jonathancho.jc@gmail.com>
2024-02-24 16:46:17 -0800

    Add user name to UserBio component.

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -29,6 +29,13 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
         )}
         <Button onClick={() => {}} label="Follow" secondary />
       </div>
+      <div className="mt-8 px4">
+        <div className="flex flex-col">
+          <p className="text-white text-2xl font-semibold">
+            {fetchedUser?.name}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
